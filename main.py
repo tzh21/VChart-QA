3b34a41e3a0fa60da891d59d84f78c3e155681b3
tzh <3136374549@qq.com>
2024-11-23 20:57:28 +0800

    adjust format

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
+from langchain_community.document_loaders import TextLoader
 from flask import Flask, request, render_template
-from qa import getQaChain
+from qa import GetQaChain, GetVectorStore
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-qaChain = getQaChain()
+base_dir = "./VChart-Doc/faq/en"  # 文档的存放目录
+documents = []
+for file in os.listdir(base_dir):
+    # 构建完整的文件路径
+    file_path = os.path.join(base_dir, file)
+    if file.endswith(".md"):
+        loader = TextLoader(file_path)
+        documents.extend(loader.load())
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+chunked_documents = text_splitter.split_documents(documents)
+
+vectorStore = GetVectorStore(chunked_documents, "http://183.172.21.164:6333")
+qaChain = GetQaChain(vectorStore)
 
 app = Flask(__name__)  # Flask APP
 
@@ -18,5 +34,4 @@ def home():
         return render_template("index.html", result=result)
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5020)
+app.run(host="0.0.0.0", debug=True, port=5020)
